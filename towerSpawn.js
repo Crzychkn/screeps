9e861981097a5630f3f1d96239dd1ef4f11b7dbf
Crzychkn <arthur.schoenfeld@gmail.com>
2017-11-28 10:03:54 -0700

    Removed towerSpawn code since it's broken for now.

--- towerSpawn.js
+++ towerSpawn.js
@@ -1,6 +1,6 @@
 var towerSpawn = {
 
-	Game.spawns['Spawn1'].room.createConstructionSite( (Game.spawns['Spawn1'].pos.x + 3), (Game.spawns['Spawn1'].pos.x + 3), STRUCTURE_TOWER );
+//	Game.spawns['Spawn1'].room.createConstructionSite( (Game.spawns['Spawn1'].pos.x + 3), (Game.spawns['Spawn1'].pos.x + 3), STRUCTURE_TOWER );
 
 };
 module.exports = towerSpawn;
