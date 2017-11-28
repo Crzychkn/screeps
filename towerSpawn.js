66960c13b15054865d192bfa271e7c727b8249fc
Crzychkn <arthur.schoenfeld@gmail.com>
2017-11-28 09:49:56 -0700

    Fixed towerSpawn code.

--- towerSpawn.js
+++ towerSpawn.js
@@ -2,5 +2,5 @@ var towerSpawn = {
 
 	Game.spawns['Spawn1'].room.createConstructionSite( (Game.spawns['Spawn1'].pos + 3), (Game.spawns['Spawn1'].pos + 3), STRUCTURE_TOWER );
 
-}
+};
 module.exports = towerSpawn;
