8e376829b5c47729ff094ac95bfcef367eae6033
Crzychkn <arthur.schoenfeld@gmail.com>
2017-11-28 09:41:13 -0700

    removed tower var.

--- towerSpawn.js
+++ towerSpawn.js
@@ -1,8 +1,6 @@
 var towerSpawn = {
 
-	var spawnPos = Game.spawns['Spawn1'].pos;
-
-	Game.spawns['Spawn1'].room.createConstructionSite( spawnPos + 5, spawnPos - 5, STRUCTURE_TOWER );
+	Game.spawns['Spawn1'].room.createConstructionSite( (Game.spawns['Spawn1'].pos + 3), (Game.spawns['Spawn1'].pos + 3), STRUCTURE_TOWER );
 
 }
 module.exports = towerSpawn;
