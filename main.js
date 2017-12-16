587e16fb3541da9a29e156415bcabcddda1ff8cf
Crzychkn <arthur.schoenfeld@gmail.com>
2017-12-15 18:43:54 -0700

    Reduced creeps.

--- main.js
+++ main.js
@@ -30,12 +30,12 @@ module.exports.loop = function () {
         console.log('Spawning new harvester: ' + newName);
     }
     
-    if(upgraders.length < 3) {
+    if(upgraders.length < 2) {
         var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
         console.log('Spawning new upgrader: ' + newName);
     }
     
-    if(builders.length < 3) {
+    if(builders.length < 2) {
         var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'builder'});
         console.log('Spawning new builder: ' + newName);
     }
