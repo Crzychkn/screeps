8a2d6b04707de82f2fdb6e32ed72e10e5943e59e
Arthur Schoenfeld <arthur@schoenfelddesign.com>
2024-01-30 19:55:48 -0700

    Revert "Added auto spawn for upgraders."
    This reverts commit ec7b62d17b82716a092a9f69f12a58539f0a2247.
    
    Undoing the auto build in modules.

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,20 +1,6 @@
 var roleUpgrader = {
   /** @param {Creep} creep **/
   run: function (creep) {
-    var harvesters = _.filter(
-      Game.creeps,
-      (creep) => creep.memory.role == "upgrader",
-    );
-    console.log("Upgrader: " + upgrader.length);
-
-    if (upgrader.length < 2) {
-      var newName = "Upgrader" + Game.time;
-      console.log("Spawning new upgrader: " + newName);
-      Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], newName, {
-        memory: { role: "upgrader" },
-      });
-    }
-
     if (creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
       creep.memory.upgrading = false;
       creep.say("🔄 harvest");
