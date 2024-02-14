6e5327329452b37cb38dfab791c1873ef678da24
Arthur Schoenfeld <arthur@schoenfelddesign.com>
2024-02-13 22:00:31 -0700

    Added new builder role code.

--- role.builder.js
+++ role.builder.js
@@ -1,31 +1,45 @@
-var roleBuilder = {
-  /** @param {Creep} creep **/
-  run: function (creep) {
-    if (creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
-      creep.memory.building = false;
-      creep.say("🔄 harvest");
-    }
-    if (!creep.memory.building && creep.store.getFreeCapacity() == 0) {
-      creep.memory.building = true;
-      creep.say("🚧 build");
-    }
-
-    if (creep.memory.building) {
-      var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-      if (targets.length) {
-        if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(targets[0], {
-            visualizePathStyle: { stroke: "#ffffff" },
-          });
+module.exports = {
+    run: function(creep) {
+        // If the creep is currently building and is out of energy, switch to harvesting mode
+        if (creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.building = false;
+            creep.say('🔄 harvest');
         }
-      }
-    } else {
-      var sources = creep.room.find(FIND_SOURCES);
-      if (creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[1], { visualizePathStyle: { stroke: "#ffaa00" } });
-      }
-    }
-  },
-};
+        // If the creep is currently harvesting and is full of energy, switch to building mode
+        if (!creep.memory.building && creep.store.getFreeCapacity() == 0) {
+            creep.memory.building = true;
+            creep.say('🚧 build');
+        }
 
-module.exports = roleBuilder;
+        // If the creep is in building mode, find construction sites and build them
+        if (creep.memory.building) {
+            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+            if (targets.length > 0) {
+                if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+            else {
+                // If there are no construction sites, switch to upgrading mode
+                creep.memory.building = false;
+                creep.say('🔄 upgrade');
+            }
+        }
+        else {
+            // If the creep is not in building mode, find energy sources and harvest them
+            var sources = creep.room.find(FIND_SOURCES);
+            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+        }
+
+        // If the creep is not building or harvesting, it must be in upgrading mode
+        if (!creep.memory.building) {
+            // Find a controller to upgrade
+            var controller = creep.room.controller;
+            if (creep.upgradeController(controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(controller, {visualizePathStyle: {stroke: '#ffffff'}});
+            }
+        }
+    }
+};
