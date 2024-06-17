2a6054c70afe4dfc2038f61f04994f23a0c63ace
Arthur Schoenfeld <arthur@schoenfelddesign.com>
2024-06-17 14:52:27 -0600

    Added additional builder.

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ module.exports.loop = function () {
   );
   console.log("Builder: " + builders.length);
 
-  if (builders.length < 3) {
+  if (builders.length < 4) {
     var newName = "Builder" + Game.time;
     console.log("Spawning new builder: " + newName);
     Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], newName, {
