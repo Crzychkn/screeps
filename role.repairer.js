886e5e5d8f8e949430c39959176b87eb46b163bf
Arthur Schoenfeld <arthur@schoenfelddesign.com>
2024-09-27 22:05:31 -0600

    Added utils file for getEnergy()

--- role.repairer.js
+++ role.repairer.js
@@ -1,3 +1,5 @@
+const utils = require("utils");
+
 module.exports = {
   run: function (creep) {
     let creepFull;
@@ -47,23 +49,7 @@ module.exports = {
       }
     } else {
       // If the creep is not in building mode, find energy sources and harvest them
-
-      let storage = creep.room.storage;
-
-      if (storage) {
-        if (creep.withdraw(storage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(storage, {
-            visualizePathStyle: { stroke: "#ffffff" },
-          });
-        }
-      } else {
-        let sources = creep.room.find(FIND_SOURCES);
-        if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(sources[0], {
-            visualizePathStyle: { stroke: "#ffffff" },
-          });
-        }
-      }
+      utils.getEnergy(creep);
     }
   },
 };
---
+++ utils.js
@@ -0,0 +1,21 @@
+function getEnergy(creep) {
+  let storage = creep.room.storage;
+  if (storage) {
+    if (creep.withdraw(storage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(storage, {
+        visualizePathStyle: { stroke: "#ffffff" },
+      });
+    }
+  } else {
+    let sources = creep.room.find(FIND_SOURCES);
+    if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(sources[0], {
+        visualizePathStyle: { stroke: "#ffffff" },
+      });
+    }
+  }
+}
+
+modules.export = {
+  getEnergy,
+};
