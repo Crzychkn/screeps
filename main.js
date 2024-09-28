cf0d39e90512b8af20ab10cb323c0975d997ee79
Arthur Schoenfeld <arthur@schoenfelddesign.com>
2024-09-28 13:13:35 -0600

    Imported tractor code.

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var roleHarvester = require("role.harvester");
 var roleUpgrader = require("role.upgrader");
 var roleBuilder = require("role.builder");
 var roleRepairer = require("role.repairer");
+var roleTractor = require("role.tractor");
 
 module.exports.loop = function () {
   //Clear memory of dead creeps
