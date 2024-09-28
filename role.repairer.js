54816f20a5da1d46483d8e81433bde7a084eeb99
Arthur Schoenfeld <arthur@schoenfelddesign.com>
2024-09-27 21:46:33 -0600

    Changed repairer to pull from storage if available.

--- role.repairer.js
+++ role.repairer.js
@@ -47,11 +47,20 @@ module.exports = {
       }
     } else {
       // If the creep is not in building mode, find energy sources and harvest them
-      if (!creepFull) {
-        var sources = creep.room.find(FIND_SOURCES);
+
+      let storage = creep.room.storage;
+
+      if (storage) {
+        if (creep.withdraw(storage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(storage, {
+            visualizePathStyle: { stroke: "#ffffff" },
+          });
+        }
+      } else {
+        let sources = creep.room.find(FIND_SOURCES);
         if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
           creep.moveTo(sources[0], {
-            visualizePathStyle: { stroke: "#ffaa00" },
+            visualizePathStyle: { stroke: "#ffffff" },
           });
         }
       }
