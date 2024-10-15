04df9bcb11c3123d9b69ea12a32bac5bc8c5d5a2
Arthur Schoenfeld <arthur@schoenfelddesign.com>
2024-10-15 10:43:00 -0600

    Prioritized extensions over containers.

--- role.harvester.js
+++ role.harvester.js
@@ -14,8 +14,8 @@ var roleHarvester = {
             (structure.structureType == STRUCTURE_TOWER ||
               structure.structureType == STRUCTURE_SPAWN ||
               structure.structureType == STRUCTURE_STORAGE ||
-              structure.structureType == STRUCTURE_CONTAINER ||
-              structure.structureType == STRUCTURE_EXTENSION) &&
+              structure.structureType == STRUCTURE_EXTENSION ||
+              structure.structureType == STRUCTURE_CONTAINER) &&
             structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
           );
         },
