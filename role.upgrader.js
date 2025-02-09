e4be796572ba62768edaf23e03362d7d8f1c08a3
Arthur Schoenfeld <arthur@schoenfelddesign.com>
2025-02-09 13:43:36 -0700

    Room Controller Signing.
    Sign room controller with upgrader creep.

--- role.upgrader.js
+++ role.upgrader.js
@@ -3,21 +3,36 @@ const utils = require("utils");
 var roleUpgrader = {
   /** @param {Creep} creep **/
   run: function (creep) {
+
+    // Check for sign
+    const controller = creep.room.controller;
+
+    if (controller) {
+      const newSign = "New player learning.";
+
+      // Check existing sign
+      if (!controller.sign || controller.sign.text !== newSign) {
+        if (creep.signController(controller, newSign) === ERR_NOT_IN_RANGE) {
+          creep.moveTo(controller);
+        }
+      }
+    }
+
     //If creep is set to upgrade, but has no energy, change memory to harvest energy.
-    if (creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
+    if (creep.memory.upgrading && creep.store[RESOURCE_ENERGY] === 0) {
       creep.memory.upgrading = false;
       creep.say("🔄 harvest");
     }
 
     //If creep is not set to upgrade, but has full energy, set to upgrade.
-    if (!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
+    if (!creep.memory.upgrading && creep.store.getFreeCapacity() === 0) {
       creep.memory.upgrading = true;
       creep.say("⚡ upgrade");
     }
 
     //If creep is set to upgrade, find controller and move to it.
     if (creep.memory.upgrading) {
-      if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+      if (creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE) {
         creep.moveTo(creep.room.controller, {
           visualizePathStyle: { stroke: "#ffffff" },
         });
