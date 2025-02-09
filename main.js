336bcf9121f1af67b15e4fb32f9c5d793088dff1
Arthur Schoenfeld <arthur@schoenfelddesign.com>
2025-02-08 22:12:11 -0700

    Add CPU unlocked status and label, fix type coercion.
    Add output and label to cpu unlocked status  output, and fix type coercion issue.

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-var roleHarvester = require("role.harvester");
-var roleUpgrader = require("role.upgrader");
-var roleBuilder = require("role.builder");
-var roleRepairer = require("role.repairer");
-var roleTractor = require("role.tractor");
+let roleHarvester = require("role.harvester");
+let roleUpgrader = require("role.upgrader");
+let roleBuilder = require("role.builder");
+let roleRepairer = require("role.repairer");
+let roleTractor = require("role.tractor");
+
+let name;
 
 module.exports.loop = function () {
+
+  // Declare variables
+  let newName;
+
   //Clear memory of dead creeps
-  for (var name in Memory.creeps) {
+  for (name in Memory.creeps) {
     if (!Game.creeps[name]) {
       delete Memory.creeps[name];
       console.log("Clearing non-existing creep memory:", name);
@@ -15,34 +21,36 @@ module.exports.loop = function () {
 
   const bucket = Game.cpu.bucket;
   const tickLimit = Game.cpu.tickLimit;
+  const cpuUnlocked = Game.cpu.unlocked;
 
   console.log('CPU Bucket: ' + bucket);
   console.log('CPU Tick Limit: ' + tickLimit);
+  console.log('CPU Unlocked Status' + cpuUnlocked)
 
   //Harvesters auto spawn
-  var harvesters = _.filter(
+  let harvesters = _.filter(
     Game.creeps,
-    (creep) => creep.memory.role == "harvester"
+    (creep) => creep.memory.role === "harvester"
   );
   console.log("Harvesters: " + harvesters.length);
 
   if (harvesters.length < 4) {
-    var newName = "Harvester" + Game.time;
+    newName = "Harvester" + Game.time;
     console.log("Spawning new harvester: " + newName);
     Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], newName, {
       memory: { role: "harvester" },
     });
   }
 
-  var tractors = _.filter(
+  let tractors = _.filter(
     Game.creeps,
-    (creep) => creep.memory.role == "tractor"
+    (creep) => creep.memory.role === "tractor"
   );
   console.log("Tractors: ", tractors.length);
 
   if (tractors.length < 1 && Game.gcl.level >= 3) {
     console.log("Tractor can be made.");
-    var newName = "Tractor" + Game.time;
+    newName = "Tractor" + Game.time;
     Game.spawns["Spawn1"].spawnCreep(
       [WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE],
       newName,
@@ -55,12 +63,12 @@ module.exports.loop = function () {
   //Upgraders auto spawn
   var upgraders = _.filter(
     Game.creeps,
-    (creep) => creep.memory.role == "upgrader"
+    (creep) => creep.memory.role === "upgrader"
   );
   console.log("Upgrader: " + upgraders.length);
 
   if (harvesters.length > 3 && upgraders.length < 2) {
-    var newName = "Upgrader" + Game.time;
+    newName = "Upgrader" + Game.time;
     console.log("Spawning new upgrader: " + newName);
     Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], newName, {
       memory: { role: "upgrader" },
@@ -70,12 +78,12 @@ module.exports.loop = function () {
   //Builders auto spawn
   var builders = _.filter(
     Game.creeps,
-    (creep) => creep.memory.role == "builder"
+    (creep) => creep.memory.role === "builder"
   );
   console.log("Builder: " + builders.length);
 
   if (harvesters.length > 3 && builders.length < 2) {
-    var newName = "Builder" + Game.time;
+    newName = "Builder" + Game.time;
     console.log("Spawning new builder: " + newName);
     Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], newName, {
       memory: { role: "builder" },
@@ -85,12 +93,12 @@ module.exports.loop = function () {
   //Repairers auto spawn
   var repairers = _.filter(
     Game.creeps,
-    (creep) => creep.memory.role == "repairer"
+    (creep) => creep.memory.role === "repairer"
   );
   console.log("Repairer: " + builders.length);
 
   if (harvesters.length > 3 && repairers.length < 1) {
-    var newName = "Repairer" + Game.time;
+    newName = "Repairer" + Game.time;
     console.log("Spawning new repairer: " + newName);
     Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], newName, {
       memory: { role: "repairer" },
@@ -127,21 +135,21 @@ module.exports.loop = function () {
   }
 
   //Run screeps logic
-  for (var name in Game.creeps) {
+  for (name in Game.creeps) {
     var creep = Game.creeps[name];
-    if (creep.memory.role == "harvester") {
+    if (creep.memory.role === "harvester") {
       roleHarvester.run(creep);
     }
-    if (creep.memory.role == "upgrader") {
+    if (creep.memory.role === "upgrader") {
       roleUpgrader.run(creep);
     }
-    if (creep.memory.role == "builder") {
+    if (creep.memory.role === "builder") {
       roleBuilder.run(creep);
     }
-    if (creep.memory.role == "repairer") {
+    if (creep.memory.role === "repairer") {
       roleRepairer.run(creep);
     }
-    if (creep.memory.role == "tractor") {
+    if (creep.memory.role === "tractor") {
       roleTractor.run(creep);
     }
   }
