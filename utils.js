0c7881eaab6dedd0da2b3296d440c5924e9a8cd0
Arthur Schoenfeld <arthur@schoenfelddesign.com>
2025-02-09 19:45:22 -0700

    Add todo for getEnergy.
    Add todo to eventually check that storage actually has some energy before pulling from it.

--- utils.js
+++ utils.js
@@ -1,14 +1,15 @@
 function getEnergy(creep) {
   let storage = creep.room.storage;
+  // TODO: Check that storage has enough in it here probably.
   if (storage) {
-    if (creep.withdraw(storage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+    if (creep.withdraw(storage, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
       creep.moveTo(storage, {
         visualizePathStyle: { stroke: "#ffffff" },
       });
     }
   } else {
     let sources = creep.room.find(FIND_SOURCES);
-    if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+    if (creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
       creep.moveTo(sources[0], {
         visualizePathStyle: { stroke: "#ffffff" },
       });
