f6c6a9b2d8be4d324c64c269cffba71c54b27bfc
Arthur Schoenfeld <arthur@schoenfelddesign.com>
2025-02-16 11:19:17 -0700

    Tower code debug.
    Added debugging to tower code for visibility.

--- role.tower.js
+++ role.tower.js
@@ -1,5 +1,3 @@
-const utils = require('utils');
-
 module.exports = {
     run: function(roomName) {
         // Get all towers in the room
@@ -7,11 +5,16 @@ module.exports = {
             filter: { structureType: STRUCTURE_TOWER }
         });
 
+        if (!towers) {
+            console.log("No towers available!");
+        }
+
         // Find enemy creeps in the room
         let hostiles = Game.rooms[roomName].find(FIND_HOSTILE_CREEPS);
 
         // If enemies exist, make towers attack
         if (hostiles.length > 0) {
+            console.log("Hostiles found!");
             towers.forEach(tower => tower.attack(hostiles[0])); // Attack first enemy found
         }
     }
