b6f7bc602fba42480ee42ae84e12e3c125b080a3
Arthur Schoenfeld <arthur@schoenfelddesign.com>
2025-02-16 09:00:12 -0700

    Update Tractor say messages and type coercion.
    Tweaked say messages in tractor. Also fixed type coercion.

--- role.tractor.js
+++ role.tractor.js
@@ -2,35 +2,35 @@ var roleTractor = {
   /** @param {Creep} creep **/
   run: function (creep) {
     //If creep has space, find energy and harvest
-    creep.say("Harvest.");
+    creep.say("🌾 harvest.");
     if (creep.store.getFreeCapacity() > 0) {
       var sources = creep.room.find(FIND_SOURCES);
-      if (creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+      if (creep.harvest(sources[1]) === ERR_NOT_IN_RANGE) {
         creep.moveTo(sources[1], { visualizePathStyle: { stroke: "#ffaa00" } });
       }
     } else {
       var targets = creep.room.find(FIND_STRUCTURES, {
         filter: (structure) => {
           return (
-            (structure.structureType == STRUCTURE_TOWER ||
-              structure.structureType == STRUCTURE_SPAWN ||
-              structure.structureType == STRUCTURE_STORAGE ||
-              structure.structureType == STRUCTURE_CONTAINER ||
-              structure.structureType == STRUCTURE_EXTENSION) &&
+            (structure.structureType === STRUCTURE_TOWER ||
+              structure.structureType === STRUCTURE_SPAWN ||
+              structure.structureType === STRUCTURE_STORAGE ||
+              structure.structureType === STRUCTURE_CONTAINER ||
+              structure.structureType === STRUCTURE_EXTENSION) &&
             structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
           );
         },
       });
       //If spawn, extension, or tower need energy, supply them
       if (targets.length > 0) {
-        if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+        if (creep.transfer(targets[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
           creep.moveTo(targets[0], {
             visualizePathStyle: { stroke: "#ffffff" },
           });
         }
       } else {
         creep.moveTo(Game.spawns.Spawn1.pos.x, Game.spawns.Spawn1.pos.y);
-        console.log("Stuck.");
+        console.log("🛑 stuck.");
       }
     }
   },
