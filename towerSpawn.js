656cbd219c6100409d4b7fa5b0a9358d9e96fc32
Arthur Schoenfeld <arthur@schoenfelddesign.com>
2025-02-15 20:42:50 -0700

    Refactor tower code.
    Refactor tower code to start using it better.

--- towerSpawn.js
+++
@@ -1,4 +0,0 @@
-var towerSpawn = {
-  //	Game.spawns['Spawn1'].room.createConstructionSite( (Game.spawns['Spawn1'].pos.x + 3), (Game.spawns['Spawn1'].pos.x + 3), STRUCTURE_TOWER );
-};
-module.exports = towerSpawn;
