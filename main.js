800d349cf287ecd716cd631945017ca149221a4a
Arthur Schoenfeld <arthur@schoenfelddesign.com>
2025-02-18 20:50:51 -0700

    Debug upgrader.
    Add console log to debug upgrader code.

--- main.js
+++ main.js
@@ -135,6 +135,7 @@ module.exports.loop = function () {
     });
     try {
       let room = Game.rooms['E57S36'];
+      console.log(room.controller.level);
       if (room && room.controller.level >= 3 && upgraders.length < 4) {
         Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], newName, {
           memory: { role: "upgrader" },
