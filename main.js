b9b52465db03412864076efb862f542bed892410
Arthur Schoenfeld <arthur@schoenfelddesign.com>
2025-02-20 13:11:55 -0700

    Add additional upgrader - bug fix.
    Add additional upgrader if room is developed enough.

--- main.js
+++ main.js
@@ -135,7 +135,7 @@ module.exports.loop = function () {
     });
     try {
       let room = Game.rooms['E57S36'];
-      if (room && room.controller.level >= 3 && upgraders.length < 5) {
+      if (room.controller.level >= 3 && upgraders.length <= 4) {
         Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], newName, {
           memory: { role: "upgrader" },
         });
