095b9fb2161340b1041ffa50bde9cde8c48f3b92
Arthur Schoenfeld <arthur@schoenfelddesign.com>
2025-03-04 15:50:40 -0700

    Rework harvester's priority.
    Bump tower down in priority.

--- role.harvester.js
+++ role.harvester.js
@@ -13,11 +13,11 @@ var roleHarvester = {
       let targets = creep.room.find(FIND_STRUCTURES, {
         filter: (structure) => {
           return (
-            (structure.structureType === STRUCTURE_TOWER ||
-              structure.structureType === STRUCTURE_SPAWN ||
+            (structure.structureType === STRUCTURE_SPAWN ||
               structure.structureType === STRUCTURE_STORAGE ||
               structure.structureType === STRUCTURE_EXTENSION ||
-              structure.structureType === STRUCTURE_CONTAINER) &&
+              structure.structureType === STRUCTURE_CONTAINER ||
+                structure.structureType === STRUCTURE_TOWER) &&
             structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
           );
         },
