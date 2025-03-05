0391cdd94b2a7f93200dc6753eceb726a37348a5
Arthur Schoenfeld <arthur@schoenfelddesign.com>
2025-03-05 10:04:30 -0700

    Add defender creep logic.
    Add logic for implementing defender creep for attacking hostiles.

--- role.defender.js
+++ role.defender.js
@@ -1,11 +1,20 @@
 module.exports = {
-    run: function(creep) {
-        // Defense logic here.
+    run: function (creep) {
+        try {
+            // Find hostiles in room
+            let hostiles = creep.room.find(FIND_HOSTILE_CREEPS);
+            let hostilesAmount = hostiles.length;
 
-        // Find hostiles in room
-
-        // Attack if in range
-        // Move to if not in range
-        console.log("Defender ready, but not implemented.")
+            // Attack if in range
+            if (hostilesAmount > 0) {
+                if (creep.attack(hostiles[0]) === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(hostiles[0], {
+                        visualizePathStyle: {stroke: "#ffffff"}
+                    });
+                }
+            }
+        } catch (error) {
+            console.error(error);
+        }
     }
 }
