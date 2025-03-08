0ebd8d7cc9155faa6c211ca391502a4cb7ee5855
Arthur Schoenfeld <arthur@schoenfelddesign.com>
2025-03-07 17:09:35 -0700

    Add containers to the getEnergy function V5.

--- utils.js
+++ utils.js
@@ -4,7 +4,7 @@ function getEnergy(creep) {
     filter: (structure) => {
       return (
           structure.structureType === STRUCTURE_CONTAINER &&
-              structure.store[RESOURCE_ENERGY] > 0
+              structure.store[RESOURCE_ENERGY] > creep.store.getFreeCapacity
       )
     }
   })
