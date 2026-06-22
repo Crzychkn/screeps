1f21a5af6737659136755b8eaf0940cf8dbe6f21
Arthur Schoenfeld <arthur@schoenfelddesign.com>
2026-06-22 10:56:51 -0600

    Refactor main game loop by modularizing room management, creep roles, and spawning logic into `manager.room`. Add utility functions for creep management and defense.

--- main.js
+++ main.js
@@ -1,390 +1,86 @@
-let roleHarvester = require("role.harvester");
-let roleUpgrader = require("role.upgrader");
-let roleBuilder = require("role.builder");
-let roleRepairer = require("role.repairer");
-let roleTractor = require("role.tractor");
-let roleScout = require("role.scout");
-let roleTower = require("role.tower");
+const roleHarvester = require("role.harvester");
+const roleUpgrader = require("role.upgrader");
+const roleBuilder = require("role.builder");
+const roleRepairer = require("role.repairer");
+const roleTractor = require("role.tractor");
+const roleScout = require("role.scout");
 const roleStim = require("role.emergencyStim");
 const roleDefender = require("role.defender");
-const utils = require("./utils");
 
+const roomManager = require("manager.room");
 
-let name;
+const roles = {
+  harvester: roleHarvester,
+  upgrader: roleUpgrader,
+  builder: roleBuilder,
+  repairer: roleRepairer,
+  tractor: roleTractor,
+  scout: roleScout,
+  stim: roleStim,
+  defender: roleDefender,
+};
 
-module.exports.loop = function () {
-
-  // Start Script
-  console.log('*********************');
-
-  // Declare variables
-  let newName;
-  let rcl;
-  let harvesterAmount;
-  let harvesterConfig;
-  let builderAmount;
-  let builderConfig;
-  let tractorAmount;
-  let tractorConfig;
-  let upgraderAmount;
-  let upgraderConfig;
-  let scoutAmount;
-  let scoutConfig;
-  let rooms = Object.keys(Game.rooms);
-  let currentRoom = rooms[0];
-  let currentGcl = Game.gcl.level;
-  let containerStore;
-  console.log(rooms.length, "room(s) owned.");
-  try {
-    for (let room of rooms) {
-      console.log("Room", room, "is at level:", Game.rooms[room].controller.level)
-      rcl = Game.rooms[room].controller.level;
-    }
-  } catch (error) {
-    console.log(error);
-  }
-
-  // At the beginning of your module.exports.loop function, 
-  // define storageAmount with a default value
-  let storageAmount = 0; // Add this line near your other variable declarations
-
-  // Variables for stats
-  try {
-    const bucket = Game.cpu.bucket;
-    const tickLimit = Game.cpu.tickLimit;
-    const cpuUnlocked = Game.cpu.unlocked;
-    
-    // Check if storage exists before trying to access it
-    if (Game.rooms[currentRoom].storage) {
-      storageAmount = Game.rooms[currentRoom].storage.store[RESOURCE_ENERGY];
-    }
-
-    // Stats to monitor
-    console.log('CPU Bucket: ' + bucket);
-    console.log('CPU Tick Limit: ' + tickLimit);
-    console.log('CPU Unlocked Status: ' + cpuUnlocked);
-    console.log('Storage Amount: ' + storageAmount);
-
-    // Only send notifications if storage exists
-    Game.notify(`Bucket Amount: ${bucket}`, 1440 * 60);
-    Game.notify(`CPU Tick Limit: ${tickLimit}`, 1440 * 60);
-    if (Game.rooms[currentRoom].storage) {
-      Game.notify(`Storage Level: ${storageAmount}`, 1440 * 60);
-    }
-  } catch (error) {
-    console.log(error);
-  }
-
-  // Creep amounts
-  let harvesters = _.filter(
-    Game.creeps,
-    (creep) => creep.memory.role === "harvester"
-  );
-  console.log("Harvesters: " + harvesters.length);
-
-  let tractors = _.filter(
-    Game.creeps,
-    (creep) => creep.memory.role === "tractor"
-  );
-  console.log("Tractors: ", tractors.length);
-
-  let builders = _.filter(
-    Game.creeps,
-    (creep) => creep.memory.role === "builder"
-  );
-  console.log("Builder: " + builders.length);
-
-  let upgraders = _.filter(
-    Game.creeps,
-    (creep) => creep.memory.role === "upgrader"
-  );
-  console.log("Upgrader: " + upgraders.length);
-
-  let scout = _.filter(
-    Game.creeps,
-    (creep) => creep.memory.role === "scout"
-  );
-  console.log("Scout: " + scout.length);
-
-  // Set amounts based on room controller level
-  switch (rcl) {
-    case 1:
-      harvesterAmount = 3;
-      builderAmount = 1;
-      upgraderAmount = 2;
-      tractorAmount = 0;
-      scoutAmount = 0;
-      scoutConfig = [MOVE, MOVE, MOVE, CLAIM];
-      tractorConfig = [WORK, MOVE, CARRY];
-      harvesterConfig = [WORK, MOVE, CARRY];
-      builderConfig = [WORK, MOVE, CARRY];
-      upgraderConfig = [WORK, MOVE, CARRY];
-      break;
-    case 2:
-      harvesterAmount = 4;
-      builderAmount = 2;
-      upgraderAmount = 3;
-      tractorAmount = 0;
-      tractorConfig = [WORK, MOVE, CARRY];
-      harvesterConfig = [WORK, WORK, MOVE, MOVE, CARRY, CARRY];
-      builderConfig = [WORK, MOVE, CARRY];
-      upgraderConfig = [WORK, MOVE, CARRY];
-      break;
-    case 3:
-      harvesterAmount = 4;
-      builderAmount = 2;
-      upgraderAmount = 4;
-      tractorAmount = 0;
-      tractorConfig = [WORK, MOVE, CARRY];
-      harvesterConfig = [WORK, WORK, MOVE, MOVE, CARRY, CARRY];
-      builderConfig = [WORK, WORK, MOVE, MOVE, CARRY, CARRY];
-      upgraderConfig = [WORK, MOVE, CARRY];
-      break;
-    case 4:
-      harvesterAmount = 4;
-      builderAmount = 2;
-      upgraderAmount = 4;
-      tractorAmount = 1;
-      tractorConfig = [WORK, MOVE, CARRY];
-      harvesterConfig = [WORK, WORK, MOVE, MOVE, CARRY, CARRY];
-      builderConfig = [WORK, WORK, MOVE, MOVE, CARRY, CARRY];
-      upgraderConfig = [WORK, MOVE, CARRY];
-      break;
-    case 5:
-      harvesterAmount = 5;
-      builderAmount = 2;
-      upgraderAmount = 5;
-      tractorAmount = 1;
-      tractorConfig = [WORK, MOVE, CARRY];
-      harvesterConfig = [WORK, WORK, MOVE, MOVE, CARRY, CARRY];
-      builderConfig = [WORK, WORK, MOVE, MOVE, CARRY, CARRY];
-      upgraderConfig = [WORK, MOVE, CARRY];
-      break;
-    case 6:
-      harvesterAmount = 5;
-      builderAmount = 2;
-      upgraderAmount = 5;
-      tractorAmount = 1;
-      tractorConfig = [WORK, WORK, WORK, WORK, WORK, WORK, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY];
-      harvesterConfig = [WORK, WORK, MOVE, MOVE, CARRY, CARRY];
-      builderConfig = [WORK, WORK, MOVE, MOVE, CARRY, CARRY];
-      upgraderConfig = [WORK, WORK, WORK, WORK, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY];
-      break;
-    case 7:
-      harvesterAmount = 5;
-      builderAmount = 2;
-      upgraderAmount = 5;
-      if (storageAmount > 900000 && harvesters.length >= harvesterAmount) {
-        upgraderAmount = 7;
-      }
-      tractorAmount = 1;
-      tractorConfig = [WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY];
-      harvesterConfig = [WORK, WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY];
-      builderConfig = [WORK, WORK, MOVE, MOVE, CARRY, CARRY];
-      upgraderConfig = [WORK, WORK, WORK, WORK, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY];
-      break;
-    case 8:
-      harvesterAmount = 5;
-      builderAmount = 2;
-      upgraderAmount = 5;
-      if (storageAmount > 900000 && harvesters.length >= harvesterAmount) {
-        upgraderAmount = 7;
-      }
-      tractorAmount = 1;
-      tractorConfig = [WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY];
-      harvesterConfig = [WORK, WORK, WORK, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY];
-      builderConfig = [WORK, WORK, MOVE, MOVE, CARRY, CARRY];
-      upgraderConfig = [WORK, WORK, WORK, WORK, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY];
-      break;
-  }
-
-  // Clear memory of dead creeps
-  for (name in Memory.creeps) {
+function cleanDeadCreepMemory() {
+  for (const name in Memory.creeps) {
     if (!Game.creeps[name]) {
       delete Memory.creeps[name];
       console.log("Clearing non-existing creep memory:", name);
     }
   }
+}
 
-  try {
-    for (let roomName in Game.rooms) {
-      let room = Game.rooms[roomName];
-      const repairQueue = utils.getRepairQueue(room);
-      console.log(repairQueue.length, 'structures to repair.')
-      if (repairQueue.length > 0) {
-        console.log(`Lowest structure is ${repairQueue[0].hits} out of ${repairQueue[0].hitsMax} hits.`);
-      }
+function getOwnedRooms() {
+  return Object.values(Game.rooms).filter((room) => {
+    return room.controller && room.controller.my;
+  });
+}
+
+function logCpuStats() {
+  console.log("CPU Bucket:", Game.cpu.bucket);
+  console.log("CPU Tick Limit:", Game.cpu.tickLimit);
+  console.log("CPU Unlocked Status:", Game.cpu.unlocked);
+}
+
+function runCreeps() {
+  for (const name in Game.creeps) {
+    const creep = Game.creeps[name];
+    const roleName = creep.memory.role;
+    const role = roles[roleName];
+
+    if (!role) {
+      console.log(`${creep.name} has unknown role: ${roleName}`);
+      continue;
     }
-  } catch (error) {
-    console.log(error);
-  }
 
-  // Defense creeps
-  let hostiles
-  for (let roomName in Game.rooms) {
-    hostiles = Game.rooms[roomName].find(FIND_HOSTILE_CREEPS);
-  }
-  let hostilesAmount = hostiles.length;
-  if (hostiles.length > 0) {
-    if (hostiles.length > 3) {
-      Game.notify(`${hostilesAmount} hostile(s) found in a room!`, 0);
-    }
-
-    let defenders = _.filter(Game.creeps, (creep) => {
-      creep.memory.role === "defender"
-    })
-    if (defenders.length < hostilesAmount) {
-      let newName = "Defender" + Game.time;
-      Game.spawns["Spawn1"].spawnCreep([MOVE, MOVE, MOVE, ATTACK, ATTACK, ATTACK],
-        newName, {
-          memory: {role: "defender"},
-        });
+    try {
+      role.run(creep);
+    } catch (error) {
+      console.log(`Error running ${roleName} for ${creep.name}:`, error);
     }
   }
+}
 
-  // // Check closest creep to death
-  // const myCreeps = Object.values(Game.creeps);
-  // try {
-  //   let closestToDeath = myCreeps.reduce((lowest, creep) => !lowest || creep.ticksToLive < lowest.ticksToLive ? creep : lowest, null
-  //   );
-  //   console.log(`Creep ${closestToDeath} dies in ${closestToDeath.ticksToLive}.`);
-  // } catch (error) {
-  //   console.log(error);
-  // }
+module.exports.loop = function () {
+  console.log("*********************");
 
-  // Check Extensions
-  let myExtensions;
-  try {
-    myExtensions = Game.rooms[currentRoom].find(FIND_STRUCTURES, {
-      filter: {structureType: STRUCTURE_EXTENSION},
-    });
+  cleanDeadCreepMemory();
+  logCpuStats();
 
-    console.log('Current Extensions:', myExtensions.length);
-    if (myExtensions.length > 0) {
-      containerStore = 0;
-      for (let container of myExtensions) {
-        containerStore += container.store[RESOURCE_ENERGY];
-      }
-      console.log('Current Extensions Total Energy:', containerStore);
-    }
-  } catch (error) {
-    console.log(error);
-  }
+  const ownedRooms = getOwnedRooms();
 
-  // Initial harvesters spawn
-  if (harvesters.length < harvesterAmount) {
-    newName = "Harvester" + Game.time;
-    console.log("Spawning new harvester: " + newName);
-    Game.spawns["Spawn1"].spawnCreep(harvesterConfig, newName, {
-      memory: {role: "harvester"},
-    });
-  }
+  console.log(`${ownedRooms.length} owned room(s).`);
+  console.log(`GCL: ${Game.gcl.level}`);
 
-  // TODO: Maybe check to ensure enough energy before creating this
-  // Get all containers / storage and ensure energy levels exceed 900 in total.
-  if (tractors.length < tractorAmount && containerStore > 1200) {
-    console.log("Tractor can be made.");
-    newName = "Tractor" + Game.time;
-    Game.spawns["Spawn1"].spawnCreep(
-      tractorConfig,
-      newName,
-      {
-        memory: {role: "tractor"},
-      }
-    );
-  }
-
-  // Builders auto spawn
-  if (harvesters.length >= harvesterAmount && builders.length < builderAmount) {
-    newName = "Builder" + Game.time;
-    console.log("Spawning new builder: " + newName);
-    Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], newName, {
-      memory: {role: "builder"},
-    });
-  }
-
-  // Upgraders auto spawn
-  if (harvesters.length >= harvesterAmount && builders.length >= builderAmount && upgraders.length < upgraderAmount) {
-    newName = "Upgrader" + Game.time;
-    console.log("Spawning new upgrader: " + newName);
-    Game.spawns["Spawn1"].spawnCreep(upgraderConfig, newName, {
-      memory: {role: "upgrader"},
-    });
-  }
-
-  // Repairers auto spawn
-  // let repairers = _.filter(
-  //     Game.creeps,
-  //     (creep) => creep.memory.role === "repairer"
-  // );
-  // console.log("Repairer: " + repairers.length);
-  //
-  // if (harvesters.length > 3 && repairers.length < 2) {
-  //     newName = "Repairer" + Game.time;
-  //     console.log("Spawning new repairer: " + newName);
-  //     Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], newName, {
-  //         memory: {role: "repairer"},
-  //     });
-  // }
-
-  // Scout auto spawn
-  // If no scouts
-  // and gcl level > 1
-
-  // if (scout.length < 1 && Game.gcl.level > Object.keys(Game.rooms).length && tractors.length > 0 && upgraders.length > 3 && containerStore > 1200) {
-  //     newName = "Scout" + Game.time;
-  //     console.log("Spawning new scout: ", newName);
-  //     const result = Game.spawns["Spawn1"].spawnCreep([MOVE, MOVE, CLAIM], newName, {
-  //         memory: {role: "scout"},
-  //     });
-  //     console.log(result);
-  // }
-
-  // Tower code
-  for (let roomName in Game.rooms) {
-    roleTower.run(roomName);
-  }
-
-  if (Game.spawns["Spawn1"].spawning) {
-    let spawningCreep = Game.creeps[Game.spawns["Spawn1"].spawning.name];
-    Game.spawns["Spawn1"].room.visual.text(
-      "🛠️" + spawningCreep.memory.role,
-      Game.spawns["Spawn1"].pos.x + 1,
-      Game.spawns["Spawn1"].pos.y,
-      {align: "left", opacity: 0.8}
-    );
-  }
-
-  // Run screeps logic
-  for (name in Game.creeps) {
-    var creep = Game.creeps[name];
-    if (creep.memory.role === "harvester") {
-      roleHarvester.run(creep);
-    }
-    if (creep.memory.role === "upgrader") {
-      roleUpgrader.run(creep);
-    }
-    if (creep.memory.role === "builder") {
-      roleBuilder.run(creep);
-    }
-    if (creep.memory.role === "repairer") {
-      roleRepairer.run(creep);
-    }
-    if (creep.memory.role === "tractor") {
-      roleTractor.run(creep);
-    }
-    if (creep.memory.role === "scout") {
-      roleScout.run(creep);
-    }
-    if (creep.memory.role === "stim") {
-      roleStim.run(creep);
-    }
-    if (creep.memory.role === "defender") {
-      roleDefender.run(creep);
+  for (const room of ownedRooms) {
+    try {
+      roomManager.run(room);
+    } catch (error) {
+      console.log(`Error managing room ${room.name}:`, error);
     }
   }
 
-  // End Script
-  console.log('*********************');
-  console.log('\n')
+  runCreeps();
+
+  console.log("*********************");
+  console.log("\n");
 };
---
+++ manager.room.js
@@ -0,0 +1,341 @@
+const roleTower = require("role.tower");
+
+const ROLE_PRIORITY = [
+  "harvester",
+  "builder",
+  "upgrader",
+  "tractor",
+];
+
+function bodyCost(body) {
+  return body.reduce((total, part) => total + BODYPART_COST[part], 0);
+}
+
+function getAvailableSpawn(room) {
+  const spawns = room.find(FIND_MY_SPAWNS);
+  return spawns.find((spawn) => !spawn.spawning);
+}
+
+function getRoomCreeps(room, role) {
+  return _.filter(Game.creeps, (creep) => {
+    return creep.memory.role === role && creep.memory.homeRoom === room.name;
+  });
+}
+
+function adoptLocalCreeps(room) {
+  for (const name in Game.creeps) {
+    const creep = Game.creeps[name];
+
+    if (
+      !creep.memory.homeRoom &&
+      creep.room.name === room.name &&
+      creep.room.controller &&
+      creep.room.controller.my
+    ) {
+      creep.memory.homeRoom = room.name;
+    }
+  }
+}
+
+function getStoredEnergy(room) {
+  let total = room.energyAvailable;
+
+  if (room.storage) {
+    total += room.storage.store[RESOURCE_ENERGY];
+  }
+
+  const containers = room.find(FIND_STRUCTURES, {
+    filter: (structure) => structure.structureType === STRUCTURE_CONTAINER,
+  });
+
+  for (const container of containers) {
+    total += container.store[RESOURCE_ENERGY];
+  }
+
+  return total;
+}
+
+function getDesiredCounts(room) {
+  const rcl = room.controller.level;
+  const storedEnergy = getStoredEnergy(room);
+
+  const desired = {
+    harvester: 3,
+    builder: 1,
+    upgrader: 2,
+    tractor: 0,
+  };
+
+  if (rcl >= 2) {
+    desired.harvester = 4;
+    desired.builder = 2;
+    desired.upgrader = 3;
+  }
+
+  if (rcl >= 3) {
+    desired.harvester = 4;
+    desired.builder = 2;
+    desired.upgrader = 4;
+  }
+
+  if (rcl >= 4) {
+    desired.harvester = 4;
+    desired.builder = 2;
+    desired.upgrader = 4;
+    desired.tractor = storedEnergy > 1200 ? 1 : 0;
+  }
+
+  if (rcl >= 5) {
+    desired.harvester = 5;
+    desired.builder = 2;
+    desired.upgrader = 5;
+    desired.tractor = storedEnergy > 1200 ? 1 : 0;
+  }
+
+  if (rcl >= 7 && storedEnergy > 900000) {
+    desired.upgrader = 7;
+  }
+
+  return desired;
+}
+
+function getBodiesForRole(role, rcl) {
+  if (role === "harvester") {
+    if (rcl >= 7) {
+      return [
+        [WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE],
+        [WORK, WORK, CARRY, CARRY, MOVE, MOVE],
+        [WORK, CARRY, MOVE],
+      ];
+    }
+
+    if (rcl >= 2) {
+      return [
+        [WORK, WORK, CARRY, CARRY, MOVE, MOVE],
+        [WORK, CARRY, MOVE],
+      ];
+    }
+
+    return [
+      [WORK, CARRY, MOVE],
+    ];
+  }
+
+  if (role === "builder") {
+    if (rcl >= 3) {
+      return [
+        [WORK, WORK, CARRY, CARRY, MOVE, MOVE],
+        [WORK, CARRY, MOVE],
+      ];
+    }
+
+    return [
+      [WORK, CARRY, MOVE],
+    ];
+  }
+
+  if (role === "upgrader") {
+    if (rcl >= 6) {
+      return [
+        [WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE],
+        [WORK, WORK, CARRY, CARRY, MOVE, MOVE],
+        [WORK, CARRY, MOVE],
+      ];
+    }
+
+    if (rcl >= 3) {
+      return [
+        [WORK, WORK, CARRY, CARRY, MOVE, MOVE],
+        [WORK, CARRY, MOVE],
+      ];
+    }
+
+    return [
+      [WORK, CARRY, MOVE],
+    ];
+  }
+
+  if (role === "tractor") {
+    if (rcl >= 7) {
+      return [
+        [WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE],
+        [WORK, WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE],
+        [WORK, CARRY, MOVE],
+      ];
+    }
+
+    return [
+      [WORK, CARRY, MOVE],
+    ];
+  }
+
+  return [
+    [WORK, CARRY, MOVE],
+  ];
+}
+
+function chooseBody(room, role) {
+  const bodies = getBodiesForRole(role, room.controller.level);
+
+  for (const body of bodies) {
+    if (bodyCost(body) <= room.energyAvailable) {
+      return body;
+    }
+  }
+
+  return null;
+}
+
+function spawnRole(room, role, body) {
+  const spawn = getAvailableSpawn(room);
+
+  if (!spawn) {
+    return ERR_BUSY;
+  }
+
+  if (!body || bodyCost(body) > room.energyAvailable) {
+    return ERR_NOT_ENOUGH_ENERGY;
+  }
+
+  const name = role.charAt(0).toUpperCase() + role.slice(1) + Game.time;
+
+  const result = spawn.spawnCreep(body, name, {
+    memory: {
+      role: role,
+      homeRoom: room.name,
+    },
+  });
+
+  if (result === OK) {
+    console.log(`Spawning ${role} in ${room.name}: ${name}`);
+  } else {
+    console.log(`Failed to spawn ${role} in ${room.name}: ${result}`);
+  }
+
+  return result;
+}
+
+function manageDefense(room) {
+  const hostiles = room.find(FIND_HOSTILE_CREEPS);
+
+  if (hostiles.length === 0) {
+    return;
+  }
+
+  console.log(`${hostiles.length} hostile(s) found in ${room.name}`);
+
+  if (hostiles.length > 3) {
+    Game.notify(`${hostiles.length} hostile(s) found in ${room.name}`, 0);
+  }
+
+  const defenders = getRoomCreeps(room, "defender");
+
+  if (defenders.length >= hostiles.length) {
+    return;
+  }
+
+  const body = [MOVE, MOVE, MOVE, ATTACK, ATTACK, ATTACK];
+
+  if (bodyCost(body) <= room.energyAvailable) {
+    spawnRole(room, "defender", body);
+  }
+}
+
+function manageSpawning(room) {
+  const spawn = getAvailableSpawn(room);
+
+  if (!spawn) {
+    return;
+  }
+
+  const desired = getDesiredCounts(room);
+
+  const counts = {
+    harvester: getRoomCreeps(room, "harvester").length,
+    builder: getRoomCreeps(room, "builder").length,
+    upgrader: getRoomCreeps(room, "upgrader").length,
+    tractor: getRoomCreeps(room, "tractor").length,
+  };
+
+  console.log(
+    `${room.name} creeps - Harvesters: ${counts.harvester}/${desired.harvester}, ` +
+    `Builders: ${counts.builder}/${desired.builder}, ` +
+    `Upgraders: ${counts.upgrader}/${desired.upgrader}, ` +
+    `Tractors: ${counts.tractor}/${desired.tractor}`
+  );
+
+  // Emergency recovery: if the room has no harvesters, prioritize the cheapest viable worker.
+  if (counts.harvester === 0) {
+    const emergencyBody = [WORK, CARRY, MOVE];
+
+    if (bodyCost(emergencyBody) <= room.energyAvailable) {
+      spawnRole(room, "harvester", emergencyBody);
+    }
+
+    return;
+  }
+
+  for (const role of ROLE_PRIORITY) {
+    if (counts[role] >= desired[role]) {
+      continue;
+    }
+
+    const body = chooseBody(room, role);
+
+    if (!body) {
+      continue;
+    }
+
+    spawnRole(room, role, body);
+    return;
+  }
+}
+
+function showSpawnVisual(room) {
+  const spawns = room.find(FIND_MY_SPAWNS);
+
+  for (const spawn of spawns) {
+    if (!spawn.spawning) {
+      continue;
+    }
+
+    const spawningCreep = Game.creeps[spawn.spawning.name];
+
+    if (!spawningCreep) {
+      continue;
+    }
+
+    room.visual.text(
+      "🛠️" + spawningCreep.memory.role,
+      spawn.pos.x + 1,
+      spawn.pos.y,
+      {
+        align: "left",
+        opacity: 0.8,
+      }
+    );
+  }
+}
+
+module.exports = {
+  run: function (room) {
+    if (!room.controller || !room.controller.my) {
+      return;
+    }
+
+    adoptLocalCreeps(room);
+
+    console.log(`Managing room ${room.name}, RCL ${room.controller.level}`);
+    console.log(`Energy: ${room.energyAvailable}/${room.energyCapacityAvailable}`);
+
+    try {
+      roleTower.run(room.name);
+    } catch (error) {
+      console.log(`Tower error in ${room.name}:`, error);
+    }
+
+    manageDefense(room);
+    manageSpawning(room);
+    showSpawnVisual(room);
+  },
+};
